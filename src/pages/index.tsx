ad91c7bf83522d92d0861a9fadd3064214e7f553
Rogix <12768403+rogix@users.noreply.github.com>
2022-01-05 14:40:01 -0300

    feat: setup chakra-ui

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <Heading>
+      Hello
+    </Heading>
+  )
+}
+
+export default Home
